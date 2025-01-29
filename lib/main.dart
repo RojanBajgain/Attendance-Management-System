98ce11ad4e0bf3a82a0a6ebeae8397c1bc146392
Rojan Bajgain <rojanbajgain123@gmail.com>
2025-01-29 16:37:22 +0545

    fix: added for time manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:ams/app.dart';
+import 'package:ams/feature/presentation/pages/dashboard/widget/time_manager.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:ams/services/dependency.dart' as depp;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  // await depp.init();
+  await depp.init();
+  await TimerManager().loadState();
 
   runApp(const App());
 }
