04563478ac3cb8a6da59dfde875d01d8a6110d26
Rojan Bajgain <rojanbajgain123@gmail.com>
2025-02-05 15:24:48 +0545

    added urls

--- lib/feature/data/datasource/remote/api_urls.dart
+++ lib/feature/data/datasource/remote/api_urls.dart
@@ -1,46 +1,25 @@
 class ApiUrls {
+  // static const String baseUrl = "http://192.168.254.81:8000/";
   // static const String baseUrl = "http://192.168.254.49:8000/";
+  // static const String baseUrl = "http://192.168.10.109:8000/";
+
   static const String baseUrl = "https://backend.ams.ayata.com.np/";
 
-  // static const String login = "auth/v3/login/";
   static const String login = "api/login/";
 
   static const String logout = "api/logout/";
 
-  // static const String register = "auth/v3/register/";
+  // static const String register = "auth/register/";
 
-  // static const String subject = "api/v3/subject/list/";
-  // static const String subjectDetail = "api/v3/subject/details/";
+  static const String profile = "api/profiles/";
 
-  // static const String teacher = "api/v3/teacher/list/";
+  static const String changePassword = "api/smtp/changepassword/";
 
-  // static const String bookmark = "api/v3/bookmark/list/";
-  // static const String createbookmark = "api/v3/bookmark/create/";
-  // static const String deletebookmark = "api/v3/bookmark/delete/";
+  static const String payroll = "api/payrolls/payroll/";
 
-  // static const String coursegroup = "api/v3/course-group/list/";
-  // static const String coursesubgroup = "api/v3/course/list/";
+  static const String timeoff = "api/timeoffs/timeoff/";
 
-  // static const String enrolledcourse = "api/v3/course/enrolled/";
-  // static const String detailenrolledcourse = "api/v3/course/details";
-
-  // static const String coursetoenroll = "api/v3/course/enroll/";
-
-  // static const String detailcontent = "api/v3/content/details/";
-
-  // static const String discussion = "api/v3/discussion/list/";
-  // static const String creatediscussion = "api/v3/discussion/create/";
-
-  // static const String test = "api/v3/test/list/";
-  // static const String testdetail = "api/v3/test/details/";
-  // static const String testsubmit = "api/v3/test/submit/";
-  // static const String testresult = "api/v3/test/result/";
-
-  // static const String poll = "api/v3/poll/list/";
-  // static const String createpoll = "api/v3/poll/create/";
-  // static const String deletepoll = "api/v3/poll/delete/";
-
-  // static const String questions = "api/v3/question-set/list/";
+  static const String timesheet = "api/timesheets/attendencelogs/";
 
   static const String token = "";
 }
