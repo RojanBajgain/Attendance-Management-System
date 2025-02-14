e155e0f9db611e8286f982f8a6dc47d0cd2511a2
Rojan Bajgain <rojanbajgain123@gmail.com>
2025-02-14 16:57:56 +0545

    fix: api urls

--- lib/feature/data/datasource/remote/api_urls.dart
+++ lib/feature/data/datasource/remote/api_urls.dart
@@ -1,9 +1,11 @@
 class ApiUrls {
   // static const String baseUrl = "http://192.168.254.81:8000/";
   // static const String baseUrl = "http://192.168.254.49:8000/";
-  static const String baseUrl = "http://192.168.10.109:8000/";
+  // static const String baseUrl = "http://192.168.254.4:8000/";
 
-  // static const String baseUrl = "https://backend.ams.ayata.com.np/";
+  // static const String baseUrl = "http://192.168.10.109:8000/";
+
+  static const String baseUrl = "https://backend.ams.ayata.com.np/";
 
   static const String login = "api/login/";
 
@@ -12,14 +14,22 @@ class ApiUrls {
   // static const String register = "auth/register/";
 
   static const String profile = "api/profiles/";
+  static const String profiledetail = "api/profiles/";
+
+  static const String policydetail = "api/policies/policydetail/";
 
   static const String changePassword = "api/smtp/changepassword/";
 
   static const String payroll = "api/payrolls/payroll/";
+  static const String payrolldetail = "api/payrolls/payroll/";
 
   static const String timeoff = "api/timeoffs/timeoff/";
+  static const String posttimeoff = "api/timeoffs/timeoff/";
 
   static const String timesheet = "api/timesheets/attendencelogs/";
+  static const String timesheetdetail = "api/timesheets/attendencelogs/";
 
-  static const String token = "";
+  static const String notification = "api/inbox/notifications/";
+
+  // static const String token = "";
 }
---
+++ lib/feature/data/repository/policy_repo.dart
@@ -0,0 +1,26 @@
+import 'package:ams/feature/data/datasource/remote/api_client.dart';
+import 'package:ams/feature/data/datasource/remote/api_response.dart';
+import 'package:ams/feature/data/datasource/remote/api_urls.dart';
+import 'package:ams/feature/presentation/pages/policy/model/policy_model.dart';
+
+class PolicyRepo {
+  final ApiClient apiClient;
+
+  PolicyRepo({required this.apiClient});
+
+  // Get Policy
+  Future<ApiResponse> getPolicydetail() async {
+    final token = apiClient.token;
+
+    if (token.isEmpty) {
+      throw Exception('JWT Token is missing or invalid');
+    }
+
+    final response = await ApiClient.getApi(
+      ApiUrls.policydetail,
+      token: token,
+      fromJson: (json) => PolicyModel.fromJson(json),
+    );
+    return response;
+  }
+}
