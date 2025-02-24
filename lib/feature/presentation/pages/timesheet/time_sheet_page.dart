24b54fc5ae0295f8d23943a3a535976a7647fcbb
Rojan Bajgain <rojanbajgain123@gmail.com>
2025-02-24 20:09:04 +0545

    fix: timesheets data

--- lib/feature/presentation/pages/timesheet/time_sheet_page.dart
+++ lib/feature/presentation/pages/timesheet/time_sheet_page.dart
@@ -6,6 +6,7 @@ import 'package:ams/feature/presentation/pages/timesheet/sub_view_timesheet/time
 import 'package:ams/feature/presentation/widget/components/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class TimeSheetPage extends StatefulWidget {
   const TimeSheetPage({super.key});
@@ -22,8 +23,9 @@ class _TimeSheetPageState extends State<TimeSheetPage> {
 
   @override
   void initState() {
-    timesheetcontroller.timesheet();
     super.initState();
+    timesheetcontroller.getTimesheet();
+    timesheetcontroller.clearSelectedDate();
   }
 
   @override
@@ -51,26 +53,83 @@ class _TimeSheetPageState extends State<TimeSheetPage> {
                     const Spacer(),
                     GestureDetector(
                       onTap: () async {
+                        final ThemeData datePickerTheme =
+                            Theme.of(context).copyWith(
+                          textTheme: TextTheme(
+                            bodyLarge: TextStyle(
+                              fontSize: 14.0,
+                              color: isDarkMode ? Colors.white : Colors.black,
+                            ),
+                            bodyMedium: TextStyle(
+                              fontSize: 12.0,
+                              color: isDarkMode ? Colors.white : Colors.black,
+                            ),
+                          ),
+                        );
                         DateTime? selectedDate = await showDatePicker(
                           context: context,
                           initialDate: DateTime.now(),
                           firstDate: DateTime(2000),
                           lastDate: DateTime(2100),
+                          builder: (BuildContext context, Widget? child) {
+                            return Theme(
+                              data: datePickerTheme,
+                              child: child!,
+                            );
+                          },
                         );
+
+                        if (selectedDate != null) {
+                          timesheetcontroller.selectedDate.value = selectedDate;
+                          timesheetcontroller.filterByDate(selectedDate);
+                        }
                       },
-                      child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.circular(30.0),
-                          color: Colors.grey[100],
-                        ),
-                        child: const Icon(
-                          Icons.date_range_outlined,
-                          color: Colors.black,
-                        ),
-                      ),
+                      child: Obx(() {
+                        return Container(
+                          height: 45.0,
+                          width: timesheetcontroller.selectedDate.value != null
+                              ? 165.0
+                              : 48.0,
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(70.0),
+                            color: isDarkMode
+                                ? Colors.grey.shade500
+                                : Colors.white,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.date_range_outlined,
+                                color: Colors.black,
+                              ),
+                              if (timesheetcontroller.selectedDate.value !=
+                                  null) ...[
+                                const SizedBox(width: 5),
+                                Text(
+                                  DateFormat('MMM d, yyyy').format(
+                                      timesheetcontroller.selectedDate.value!),
+                                  style:
+                                      smallStyle.copyWith(color: Colors.black),
+                                ),
+                                const SizedBox(width: 5),
+                                GestureDetector(
+                                  onTap: () {
+                                    timesheetcontroller.clearSelectedDate();
+                                  },
+                                  child: const Icon(
+                                    Icons.clear,
+                                    size: 20.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ],
+                          ),
+                        );
+                      }),
                     ),
                   ],
                 ),
@@ -103,10 +162,11 @@ class _TimeSheetPageState extends State<TimeSheetPage> {
                             shrinkWrap: true,
                             physics: const NeverScrollableScrollPhysics(),
                             scrollDirection: Axis.vertical,
-                            itemCount: timesheetcontroller.timesheet.length,
+                            itemCount:
+                                timesheetcontroller.filteredTimesheet.length,
                             itemBuilder: (context, index) {
                               final timesheet =
-                                  timesheetcontroller.timesheet[index];
+                                  timesheetcontroller.filteredTimesheet[index];
                               return Padding(
                                 padding: const EdgeInsets.all(8.0),
                                 child:
