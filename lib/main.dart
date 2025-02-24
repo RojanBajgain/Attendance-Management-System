02a150d6ff27a4c9ab8314f94152476491838341
Rojan Bajgain <rojanbajgain123@gmail.com>
2025-02-24 20:05:51 +0545

    fix: added the 'keep user logging'

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:ams/app.dart';
-import 'package:ams/feature/presentation/pages/dashboard/widget/time_manager.dart';
-
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:ams/services/dependency.dart' as depp;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   await depp.init();
-  await TimerManager().loadState();
+  // AppSettings.openAppSettings();
 
-  runApp(const App());
+  // Check login status before starting the app
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  runApp(App(isLoggedIn: isLoggedIn));
 }
