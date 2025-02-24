534b6793d999e2d187c96900726813ae53bf1b46
Rojan Bajgain <rojanbajgain123@gmail.com>
2025-02-24 20:12:45 +0545

    fix: minor changes

--- lib/feature/presentation/pages/dashboard/sub_view_dashboard/logsheet_constant.dart
+++ lib/feature/presentation/pages/dashboard/sub_view_dashboard/logsheet_constant.dart
@@ -6,19 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
-class LogSheetConstant extends StatefulWidget {
+class LogSheetConstant extends StatelessWidget {
   final Datum timesheetdata;
 
-  LogSheetConstant({
-    super.key,
-    required this.timesheetdata,
-  });
+  LogSheetConstant({super.key, required this.timesheetdata});
 
-  @override
-  State<LogSheetConstant> createState() => _LogSheetConstantState();
-}
-
-class _LogSheetConstantState extends State<LogSheetConstant> {
   final TimesheetController timesheetcontroller =
       Get.put(TimesheetController(timesheetRepo: Get.find()));
 
@@ -26,88 +18,80 @@ class _LogSheetConstantState extends State<LogSheetConstant> {
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
-    return GestureDetector(
-      onTap: () {
-        // Navigator.push(
-        //   context,
-        //   MaterialPageRoute(
-        //     builder: (context) => TimeSheetDetail(timesheetId: ,),
-        //   ),
-        // );
-      },
-      child: Container(
-        // color: isDarkMode ? Colors.black : Colors.grey,
-        height: 90.0,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0),
-          color: isDarkMode ? Colors.grey.shade800 : Colors.grey.shade200,
-          gradient: LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [
-              isDarkMode ? Colors.grey : Colors.black,
-              isDarkMode ? Colors.grey : Colors.black,
-              isDarkMode ? Colors.grey.shade800 : Colors.grey.shade200,
-            ],
-            stops: [
-              0.0,
-              0.04,
-              0.0,
-            ],
-          ),
+    return Container(
+      // color: isDarkMode ? Colors.black : Colors.grey,
+      height: 90.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: isDarkMode ? Colors.grey.shade800 : Colors.grey.shade200,
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [
+            isDarkMode ? Colors.grey : Colors.black,
+            isDarkMode ? Colors.grey : Colors.black,
+            isDarkMode ? Colors.grey.shade800 : Colors.grey.shade200,
+          ],
+          stops: const [
+            0.0,
+            0.04,
+            0.0,
+          ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 30.0,
-            top: 15.0,
-            right: 20.0,
-          ),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Sunday, 01 Nov 2024',
-                style: TextStyle(
-                  fontFamily: 'Mukta',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 15.0,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 30.0,
+          top: 15.0,
+          right: 20.0,
+        ),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Sunday, 01 Nov 2024',
+              style: TextStyle(
+                fontFamily: 'Mukta',
+                fontWeight: FontWeight.w600,
+                fontSize: 15.0,
+                color: isDarkMode ? Colors.white : Colors.black,
+              ),
+            ),
+            const SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.history,
                   color: isDarkMode ? Colors.white : Colors.black,
                 ),
-              ),
-              const SizedBox(height: 15.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(
-                    Icons.history,
-                    color: isDarkMode ? Colors.white : Colors.black,
-                  ),
-                  // SizedBox(width: 5.0),
-                  Text('9:30 AM',
-                      style: smallStyle.copyWith(color: Colors.green)),
-                  // const SizedBox(width: 25.0),
-                  Icon(
-                    Icons.update,
-                    color: isDarkMode ? Colors.white : Colors.black,
-                  ),
-                  // SizedBox(width: 5.0),
-                  Text('5:30 AM',
-                      style: smallStyle.copyWith(color: Colors.redAccent)),
-                  // const SizedBox(width: 25.0),
-                  Icon(
-                    Icons.schedule,
-                    color: isDarkMode ? Colors.white : Colors.black,
-                  ),
-                  // SizedBox(width: 5.0),
-                  Text('8h 50min',
-                      style: smallStyle.copyWith(
-                          color: isDarkMode ? Colors.white : Colors.black))
-                ],
-              ),
-            ],
-          ),
+                // SizedBox(width: 5.0),
+                Text(
+                  '9:30 AM',
+                  style: smallStyle.copyWith(color: Colors.green),
+                ),
+                // const SizedBox(width: 25.0),
+                Icon(
+                  Icons.update,
+                  color: isDarkMode ? Colors.white : Colors.black,
+                ),
+                // SizedBox(width: 5.0),
+                Text('5:30 AM',
+                    style: smallStyle.copyWith(color: Colors.redAccent)),
+                // const SizedBox(width: 25.0),
+                Icon(
+                  Icons.schedule,
+                  color: isDarkMode ? Colors.white : Colors.black,
+                ),
+                // SizedBox(width: 5.0),
+                Text('8h 50min',
+                    style: smallStyle.copyWith(
+                        color: isDarkMode ? Colors.white : Colors.black))
+              ],
+            ),
+          ],
         ),
       ),
     );
