e75da65ddc9b4d4cd4b8141e74b47f824ee76126
Aadarsha bajagain <aadarshabajagain@Aadarshas-MacBook-Pro.local>
2025-08-07 17:39:41 +0545

    added ios icon

---
+++ environment.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static String get fileName {
+    // return '.env.development';
+    // return '.env.production';
+
+    return '.env.development'; // Replace with your desired default or logic
+  }
+
+  static String get apiBaseUrl {
+    return dotenv.env['API_BASE_URL'] ?? "API_BASE_URL not specified";
+  }
+}
